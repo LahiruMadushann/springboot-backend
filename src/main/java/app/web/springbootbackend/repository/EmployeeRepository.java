40a4f1ea5a078cb76b0b2295950524216ff3630e
Lahiru Madushanka <lahirumadushandl@gmail.com>
2022-12-25 20:23:14 +0530

    Initial commit

---
+++ src/main/java/app/web/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package app.web.springbootbackend.repository;
+
+import app.web.springbootbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
